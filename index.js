cd2e7fb762b884ec55bbdb01993a46ed9f6b43d0
Cesar adrian Hernández Hernández <cesarahh3@gmail.com>
2024-05-24 20:07:08 -0600

    Routes of api

--- index.js
+++ index.js
@@ -1,43 +1,15 @@
-const express = require('express');
-const mysql = require('mysql');
+const express = require('express')
+const bodyParser = require('body-parser');
 const cors = require('cors');
-
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1111',
-    database: 'db-choco'
-});
+const userRoutes = require('./routes/user')
+app.use('/api/users', userRoutes) 
 
-app.post('/login', (req, res) => {
-    const sqlUserCheck = "SELECT email, password, token FROM users WHERE email = ?";
-    const valuesUserCheck = [req.body.email];
-
-    db.query(sqlUserCheck, valuesUserCheck, (err, data) => {
-        if (err) {
-            return res.status(500).json({ error: "Error del servidor" });
-        }
-
-        if (data.length === 0) {
-            return res.status(404).json({ error: "Usuario no encontrado" });
-        }
-
-        const user = data[0];
-
-        if (user.password !== req.body.password) {
-            return res.status(401).json({ error: "Contraseña incorrecta" });
-        }
-
-        // Excluir la password de la respuesta
-        const { username, token } = user;
-        return res.status(200).json({ username, token });
-    });
-});
 
 app.listen(8000, () => {
     console.log("En línea...");
