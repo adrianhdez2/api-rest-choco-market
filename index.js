8001073bc5cc41e585163c87c402e5115b334941
Cesar adrian Hernández Hernández <cesarahh3@gmail.com>
2024-05-26 12:55:04 -0600

    Add configuration

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express')
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}))
-
+const cookieParser = require('cookie-parser')
 const userRoutes = require('./routes/user')
-app.use('/api/users', userRoutes) 
+
+app.use(cookieParser())
+app.use(express.json());
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    credentials: true
+}));
+
+app.use('/api/users', userRoutes)
 
 
 app.listen(8000, () => {
