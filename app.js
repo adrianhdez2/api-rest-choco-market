f4018bfe998fce2548a6d667b8e933e0862da0c9
Cesar adrian Hernández Hernández <cesarahh3@gmail.com>
2024-06-01 21:55:30 -0600

    Add file upload

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ import { usersRuter } from './routes/users.js'
 import { corsMiddleware } from './middlewares/cors.js'
 import { authRouter } from './routes/auth.js'
 import cookieParser from 'cookie-parser'
+import fileUpload from 'express-fileupload'
+
 const app = express()
 
 app.use(json())
 app.use(cookieParser())
 app.disable('x-powered-by')
 app.use(corsMiddleware())
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: '/temp/'
+}))
 app.use('/users', usersRuter)
 app.use('/auth', authRouter)
 
