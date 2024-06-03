bf8479d73065650e2e1f361280e46c2d68533de1
Cesar adrian Hernández Hernández <cesarahh3@gmail.com>
2024-06-02 19:16:03 -0600

    Translate errors

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,20 +3,28 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
 import crypto from 'crypto'
-import cloudinary from "../configs/clodinary.js"
+import { deleteCloudinaryResource } from "../utils/deleteResource.js"
+import Randomstring from "randomstring"
 
 dotenv.config()
+
+function generateOTP() {
+    return Randomstring.generate({
+        length: 6,
+        charset: 'numeric'
+    });
+}
 
 export class AuthController {
     static async getLoginToken(req, res) {  // --> Obtener el token al hacer el login
         const { email, password } = req.body
         const user = await AuthModel.getUserByEmail({ email })
 
-        if (!user) return res.status(404).json({ error: 'Email not found' })
+        if (!user) return res.status(404).json({ error: 'Este correo no existe' })
 
         const passwordMatch = await bcrypt.compare(password, user.password)
 
-        if (!passwordMatch) return res.status(401).json({ error: 'Invalid password' })
+        if (!passwordMatch) return res.status(401).json({ error: 'La contraseña no es correcta' })
 
         const token = jwt.sign({ id: user.user_id }, process.env.KEY, { expiresIn: '10d' })
         res.cookie("token", token, { httpOnly: true, maxAge: 10 * 24 * 60 * 60 * 1000, secure: true })
@@ -52,7 +60,32 @@ export class AuthController {
         } catch (error) {
             return res.status(401).json({ error: "Ocurrió un error al enviar el correo" })
         }
+    }
 
+    static async sendEmailOTP(req, res) {
+        try {
+            const token = req.cookies.token
+            const decoded = jwt.verify(token, process.env.KEY)
+            const id = decoded.id
+            const user = await AuthModel.getById({ id })   
+
+            if (!user) return res.status(401).json({ error: "No hay usuario" })
+
+            const email = user.email
+            const otp = generateOTP()
+            const expires_at = new Date(Date.now() + 5 * 60 * 1000);
+
+            const userInf = await AuthModel.saveOTP({email, otp, expires_at})
+
+            if (!userInf) return res.status(401).json({error: "Ocurrio un error al guardar el código"})
+
+            await AuthModel.sendOTP({ email, otp})
+
+            res.json({ status: true, message: "Se envió correctamente" });
+
+        } catch (error) {
+            return res.status(401).json({ error: "Ocurrió un error al enviar el correo" })
+        }
     }
 
     static async createNewUser(req, res) { // --> Crear un nuevo usuario
@@ -87,18 +120,12 @@ export class AuthController {
 
         const userInf = await AuthModel.getById({ id })
 
-        if (!userInf) return res.status(401).json({error: "No hay usuario"})
+        if (!userInf) return res.status(401).json({ error: "No hay usuario" })
 
         const public_id_picture = userInf.public_id_picture
 
         if (public_id_picture !== 'default') {
-            cloudinary.api.delete_resources(public_id_picture, { type: 'upload', resource_type: 'image' }, (error, results) => {
-                if (error) {
-                    console.log(error)
-                } else {
-                    console.log(results);
-                }
-            })
+            await deleteCloudinaryResource(public_id_picture)
         }
 
         const user = AuthModel.deleteAccount({ id })
